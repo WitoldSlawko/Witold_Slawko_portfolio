c139ee053f3bc2fc9e2b80aff2a2f703c1d81e97
WitoldSlawko <witold.slawko@gmail.com>
2017-08-12 10:17:52 +0200

    small updates

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ const About = {
 						 I'm an ambitious code passionate, <br>
 						 with strong analytical and problem solving skills. <br> 
 						 Everyday I seek to develop my skills higher. <br>
- 						 Also, I'm a very communicative  and friendly person.
+ 						 Also, I'm a very communicative  and open minded person.
 						<br><br>
 						At the moment I'm learning more about ES6, ReactJS & Vue.js.<br>
 						Also currently participating in JS Up Skill<br>
